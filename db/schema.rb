93fcfa9beb2bc8ab5b1185711ea5b590d533c6b3
Preston Copeland <copelandps1@gmail.com>
2015-06-18 21:04:09 -0400

    add first name and last name to user sign in, change devise views and created devise permitted parameters in config initializers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150618231706) do
+ActiveRecord::Schema.define(version: 20150619004858) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -80,6 +80,8 @@ ActiveRecord::Schema.define(version: 20150618231706) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "role"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

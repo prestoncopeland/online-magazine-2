cb8911df2ff778713117aeef9a68c76765e788cc
Preston Copeland <copelandps1@gmail.com>
2015-07-19 11:47:20 -0400

    change payola migration to fix missing tables error

--- db/migrate/20150703221544_remove_payola_tables.rb
+++ db/migrate/20150703221544_remove_payola_tables.rb
@@ -3,10 +3,5 @@ class RemovePayolaTables < ActiveRecord::Migration
     remove_index :users, column: :plan_id
     remove_column :users, :plan_id, :integer
     drop_table :plans
-    drop_table :payola_subscriptions
-    drop_table :payola_stripe_webhooks
-    drop_table :payola_sales
-    drop_table :payola_affiliates
-    drop_table :payola_coupons
   end
 end
